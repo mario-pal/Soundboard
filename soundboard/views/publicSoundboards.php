3d177cf4e2197b033596038650bc56164c5df008
MarioPal <mario@mariopals-mbp.attlocal.net>
2019-05-26 00:56:34 -0700

    fixed database access, update navtabs for bootstrap 4.3 and including javascript for pagination in publisSoundboards.php

--- soundboard/views/publicSoundboards.php
+++ soundboard/views/publicSoundboards.php
@@ -7,7 +7,7 @@
 	if(isset($_GET["page"])){ $page = $_GET["page"]; } else { $page=1; };
 	$start_from = ($page-1) * $limit;
 
-	$sql = "SELECT * FROM `soundboard` WHERE public = 1 ORDER
+	$sql = "SELECT * FROM 'soundboards' WHERE public = 1 ORDER
 	BY soundboard_id ASC LIMIT $start_from, $limit";
 
 	$rs_result = mysqli_query($conn, $sql);
@@ -22,12 +22,12 @@
     <meta name="author" content="Mario">
     <title>Public Soundboards</title>
     <!---->
-    <link href="../css/bootstrap.min.css" rel="stylesheet">
-    <link href="../css/jumbotron-narrow.css" rel="stylesheet">
+    <link href="bootstrap/dist/css/bootstrap.min.css" rel="stylesheet">
+    <link href="bootstrap/dist/css/jumbotron-narrow.css" rel="stylesheet">
 
-    <script type = "text/javascript" charset = "utf8" src = "../js/jquery-2.0.3.js"></script>
-    <script type = "text/javascript" src = "../js/bootstrap.min.js"></script>
-    <script src = "../js/paginationFunc.js"></script>
+    <script type = "text/javascript" charset = "utf8" src = "../../js/jquery-2.0.3.js"></script>
+    <script type = "text/javascript" src = "bootstrap/dist/js/bootstrap.min.js"></script>
+    <script src = "../../js/paginationFunc.js"></script>
   </head>
   <body>
     <br>
@@ -35,21 +35,21 @@
       <div class = "header clearfix">
       <nav>
         <div class = pull-left> Public Soundboards </div>
-        <ul class="nav nav-tabs pull-right">
-	  <li role="presentation"><a href="../index.php">Home</a></li>
+        <ul class="nav nav-tabs">
+	  <li class = "nav-item"><a class=/"nav-link/" href="../index.php">Home</a></li>
 	    <?php
 	    	if(isset($_SESSION['user'])){
-			echo "<li role=\"presentation\"><a href=\"./dashboard.php\">";
+			echo "<li class=\"nav-item\"><a class=\"nav-link\" href=\"./dashboard.php\">";
 			echo $_SESSION['user'];
 			echo "'s Dashboard</a></li> \n";
-			echo "<li role=\"presentation\"><a
+			echo "<li class=\"nav-item\"><a class=\"nav-link\"
 			href=\"../model/processLogout.php\">";
 			echo "Logout</a></li> \n";
 		}
 		else{
-			echo "<li role=\"presentation\"><a
+			echo "<li class=\"nav-item\"><a class=\"nav-link\"
 			href=\"./login.php\">Login</a></li> \n";
-			echo "<li role=\"presentation\"><a
+			echo "<li class=\"nav-item\"><a class=\"nav-link\"
 			href=\"./registration.php\">Sign up</a></li> \n";
                 }
 	    ?>
@@ -93,7 +93,7 @@
       </table>
 
     <?php
-    $sql = "SELECT COUNT(soundboard_id) FROM `soundboard` WHERE public = 1";
+    $sql = "SELECT COUNT(soundboard_id) FROM `soundboards` WHERE public = 1";
     $rs_result = mysqli_query($conn, $sql);
     $row = mysqli_fetch_row($rs_result);
     $total_records = $row[0];
