03b29fbf30838b7be0c19e995ea9b601a36b566f
MarioPal <mario@mariopals-mbp.attlocal.net>
2019-05-31 20:03:20 -0700

    Implemented registration page

--- soundboard/views/registration.php
+++ soundboard/views/registration.php
@@ -1,7 +1,7 @@
 <?php
 	session_start();
 	if ( isset($_SESSION['user']) && $_SESSION['isAdmin'] == false ){
-		header("Location: ../index.php");
+		header("Location: ../../index.php");
 		die();
 	}
 
