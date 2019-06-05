2363ef3af4bcc46ac62bc935b8559b8c5e11d07e
MarioPal <mario@mariopals-mbp.attlocal.net>
2019-06-05 02:39:10 -0700

    the login page now redirects the user to the home page if the user is already logged in but accessed the login page wia url

--- soundboard/views/login.php
+++ soundboard/views/login.php
@@ -1,5 +1,10 @@
 <?php
 session_start();
+
+ if( isset($_SESSION['user']) && $_SESSION['isAdmin'] == false ){
+      header("Location: ../../index.php");
+      die();
+    }
 ?>
 <!DOCTYPE html>
 <html lang="en">
