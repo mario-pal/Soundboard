a1737fab8b2623e2ff848d80208f53beac0cb569
MarioPal <mario@mariopals-mbp.attlocal.net>
2019-06-16 14:35:06 -0700

    fixed error where logout and login attempts where not recorded by modifying sql queries

--- soundboard/model/processLogout.php
+++ soundboard/model/processLogout.php
@@ -4,7 +4,7 @@
 
 	//log first
 	$username = $_SESSION['user'];
-	$sql = "UPDATE users SET logouts = logouts + 1 WHERE username = '$username'";
+	$sql = "UPDATE users SET logouts = logouts + 1 WHERE user_name = '$username'";
 	mysqli_query($conn, $sql);
 
 
