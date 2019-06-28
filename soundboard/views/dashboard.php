ae68d21848e665261865b1f056efca2159e38995
MarioPal <mario@mariopals-mbp.attlocal.net>
2019-06-28 01:08:39 -0700

    fixed pagination on dashboard page

--- soundboard/views/dashboard.php
+++ soundboard/views/dashboard.php
@@ -136,7 +136,7 @@
 	  $total_pages = ceil($total_records / $limit);
 	  $pagLink = "<nav><ul class=\"pagination justify-content-center\">";
 	  for ($i = 1; $i<=$total_pages; $i++){
-		  $pagLink .= "<li class=\"page-item\" ><a class=\"page-link\" href='publicSoundboards.php?page=".$i."'>".$i."</a></li>";
+		  $pagLink .= "<li class=\"page-item\" ><a class=\"page-link\" href='dashboard.php?page=".$i."'>".$i."</a></li>";
 	  };
 	  echo $pagLink . "</ul></nav>";
 	?>
